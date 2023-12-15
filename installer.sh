37afd10a01c64a6de25e2b37e766022163bb8ba7
JamesCMorey <jmorey@tutanota.com>
2023-12-15 15:56:40 -0600

    added auto-update for debian based distros (haven't checked functionality of installer changes)

--- installer.sh
+++ installer.sh
@@ -4,4 +4,6 @@ ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/.ctags ~/.ctags
 
+sudo ln -s $(pwd)/auto-update /etc/cron.daily/
+
 sudo apt update && sudo apt install $(cat packages.txt) -y
