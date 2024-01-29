dfa9ca3b45c16dae1920dd0e5397f951679c68d6
JamesCMorey <jmorey@tutanota.com>
2024-01-28 18:01:36 -0600

    Fixed error where .vim wouldn't be symlinked and remap collision in vimrc

--- linker.zsh
+++ linker.zsh
@@ -2,10 +2,12 @@
 
 
 read -p "This script assumes you are in the dotfiles repo. Press enter to continue... " input
-ln -s $(pwd)/files/vimrc ~/.vimrc
+
+ln -s $(pwd)/vim ~/.vim
 mkdir -p ~/.vim/swp
 mkdir -p ~/.vim/backup
+
+ln -s $(pwd)/files/vimrc ~/.vimrc
 ln -s $(pwd)/files/tmux.conf ~/.tmux.conf
 ln -s $(pwd)/files/ctags ~/.ctags
 ln -s $(pwd)/files/zshrc ~/.zshrc
-ln -s $(pwd)/vim ~/.vim
