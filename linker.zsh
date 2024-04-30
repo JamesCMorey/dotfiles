ee9a42f2b5e7cc2b51c167fdc3338d6660cfa54f
JamesCMorey <jmorey@tutanota.com>
2024-04-30 07:51:09 -0500

    changed default to bash and added auto plug-install

--- linker.zsh
+++ linker.zsh
@@ -3,13 +3,16 @@
 
 read -p "This script assumes you are in the dotfiles repo. Press enter to continue... " input
 
-# mkdir $(pwd)/vim
-# ln -s $(pwd)/vim ~/.vim
-# mkdir -p ~/.vim/swp
-# mkdir -p ~/.vim/backup
-# mkdir -p ~/.vim/undodir
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s $(pwd)/files/vimrc ~/.vimrc
+ mkdir $(pwd)/vim
+# ln -s $(pwd)/vim ~/.vim
+ mkdir -p ~/.vim/swp
+ mkdir -p ~/.vim/backup
+ mkdir -p ~/.vim/undodir
+
+ln -s $(pwd)/files/bashrc ~/.bashrc
 ln -s $(pwd)/files/tmux.conf ~/.tmux.conf
 ln -s $(pwd)/files/ctags ~/.ctags
 ln -s $(pwd)/files/zshrc ~/.zshrc
