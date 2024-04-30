69e8dc2be0089e61e77291aa02c0ee62af5a34d1
JamesCMorey <jmorey@tutanota.com>
2024-04-30 00:41:47 -0500

    removing .vim from linker.zsh

--- linker.zsh
+++ linker.zsh
@@ -3,9 +3,11 @@
 
 read -p "This script assumes you are in the dotfiles repo. Press enter to continue... " input
 
-ln -s $(pwd)/vim ~/.vim
-mkdir -p ~/.vim/swp
-mkdir -p ~/.vim/backup
+# mkdir $(pwd)/vim
+# ln -s $(pwd)/vim ~/.vim
+# mkdir -p ~/.vim/swp
+# mkdir -p ~/.vim/backup
+# mkdir -p ~/.vim/undodir
 
 ln -s $(pwd)/files/vimrc ~/.vimrc
 ln -s $(pwd)/files/tmux.conf ~/.tmux.conf
